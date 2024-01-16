71b4e45f65df44703bc65de73d3e918e2849ef33
gaojie <2418728171@qq.com>
2024-01-16 19:58:44 +0800

    链表

--- chapter02/LinkedList.java
+++ chapter02/LinkedList.java
@@ -7,33 +7,148 @@ package chapter02;
  * @description: 线性表的实现：链表
  */
 public class LinkedList<T> implements MyList<T> {
-    @Override
-    public int indexOf(Object o) {
-        return 0;
+    Node firstNode = null;
+
+    static class Node {
+        Object data;
+        Node next = null;
+    }
+
+    LinkedList() {
     }
 
     @Override
+    public int indexOf(Object o) {
+        if (firstNode == null) {
+            return -1;
+        }
+        Node tempNode = firstNode;
+        int index = -1;
+        do {
+            if (o.equals(tempNode.data)) {
+                return ++index;
+            }
+            ++index;
+            tempNode = tempNode.next;
+        } while (tempNode.next != null);
+        return -1;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
     public T get(int index) {
+        Node temNode = getNode(index);
+        if (temNode == null) {
+            return null;
+        } else {
+            return (T) temNode.data;
+        }
+    }
+
+    /**
+     * 获取index下标的元素节点Node
+     *
+     * @param index 待获取位置下标
+     * @return 该下标的值Node，没有该下标的值时返回null。
+     */
+    private Node getNode(int index) {
+        if (index < 0) {
+            return null;
+        }
+        int i = 0;
+        Node temNode = firstNode;
+        do {
+            if (i == index) {
+                return temNode;
+            }
+            ++i;
+            temNode = temNode.next;
+        }
+        while (temNode.next != null);
+        if (i == index) {
+            return temNode;
+        }
         return null;
     }
 
     @Override
     public void add(int index, T value) {
-
+        // 创建新的节点
+        Node dataNode = new Node();
+        dataNode.data = value;
+        // 插入位置为第一个节点
+        if (index == 0) {
+            dataNode.next = firstNode;
+            firstNode = dataNode;
+            return;
+        }
+        // 获取待插入节点的上一个节点
+        Node temNode = getNode(index - 1);
+        if (temNode != null) { // 插入
+            dataNode.next = temNode.next;
+            temNode.next = dataNode;
+        } else {
+            System.out.println("index参数错误");
+        }
     }
 
     @Override
     public void add(T value) {
-
+        if (firstNode == null) {
+            firstNode = new Node();
+            firstNode.data = value;
+        } else {
+            Node temNode = firstNode;
+            while (temNode.next != null) {
+                temNode = temNode.next;
+            }
+            temNode.next = new Node();
+            temNode.next.data = value;
+        }
     }
 
     @Override
     public void delete(int index) {
-
+        if (index < 0) {
+            return;
+        }
+        if (index == 0) {
+            firstNode = firstNode.next;
+        }
+        Node temNode = getNode(index - 1);
+        if (temNode == null || temNode.next == null) {
+            return;
+        }
+        temNode.next = temNode.next.next;
     }
 
     @Override
     public int size() {
-        return 0;
+        if (firstNode == null) {
+            return 0;
+        }
+        int count = 1;
+        Node temNode = firstNode;
+        while (temNode.next != null) {
+            ++count;
+            temNode = temNode.next;
+        }
+        return count;
+    }
+
+    @Override
+    public String toString() {
+        if (firstNode == null) {
+            return null;
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        Node temNode = firstNode;
+        while (temNode.next != null) {
+            sb.append(temNode.data).append(",");
+            temNode = temNode.next;
+        }
+        sb.append(temNode.data).append("]");
+        return sb.toString();
     }
 }
--- chapter02/Main.java
+++ chapter02/Main.java
@@ -9,6 +9,11 @@ package chapter02;
  */
 public class Main {
     public static void main(String[] args) {
+        testSequentialList();
+        testLinkedList();
+    }
+
+    public static void testSequentialList() {
         MyList<Integer> sequentialList = new SequentialList<>();
         sequentialList.add(1);
         sequentialList.add(2);
@@ -21,11 +26,23 @@ public class Main {
         System.out.println(sequentialList);
         sequentialList.delete(4);
         System.out.println("4所属的下标" + sequentialList.indexOf(4));
-        System.out.println("长度" + sequentialList.size());
+        System.out.println("顺序表的长度" + sequentialList.size());
         System.out.println("下标1的元素值：" + sequentialList.get(1));
         System.out.println(sequentialList);
+    }
 
+    public static void testLinkedList() {
         MyList<Integer> linkedList = new LinkedList<>();
-        System.out.println(linkedList.size());
+        linkedList.add(1);
+        linkedList.add(2);
+        System.out.println(linkedList);
+        linkedList.add(0, 0);
+        linkedList.add(3);
+        System.out.println(linkedList);
+        linkedList.delete(2);
+        System.out.println(linkedList);
+        System.out.println("下标0的值：" + linkedList.get(0));
+        System.out.println("下标1的值为：" + linkedList.indexOf(1));
+        System.out.println("链表的长度：" + linkedList.size());
     }
 }
